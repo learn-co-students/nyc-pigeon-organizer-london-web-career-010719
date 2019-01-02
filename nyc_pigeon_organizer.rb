0e9b4864b3fb9a418c41e057361e4853852468a4
Joseph O'Dell <josephodell@outlook.com>
2019-01-02 13:31:48 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+pidgeon_list = {}
+
+  data.each do |attribute, values|
+    values.each do |valuekey, names|
+      names.each do |name|
+        pidgeon_list[name] = {} if pidgeon_list[name] == nil
+        pidgeon_list[name][attribute] = [] if pidgeon_list[name][attribute] == nil
+        pidgeon_list[name][attribute] << valuekey.to_s
+      end
+    end
+  end
+
+pidgeon_list
 end
