f22677d2f4c6f1d6e88e9cdbef61f031214cae1e
Songyishu Yang <songyishu.yang@gmail.com>
2019-01-06 16:43:53 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = Hash.new
+  data.each do |data_type, data|
+    data.each do |quality, names|
+      names.each do |name|
+        if pigeon_list[name]
+          if pigeon_list[name][data_type]
+            pigeon_list[name][data_type] << quality.to_s
+          else
+            pigeon_list[name][data_type] = [quality.to_s]
+          end
+        else
+          pigeon_list[name] = {data_type => [quality.to_s]}
+        end
+      end
+    end
+  end
+  pigeon_list
 end
